f528870502c4032f9ba19587c845d969041fef51
Alberto Alvarado Jr <aajrgs@gmail.com>
2022-09-12 16:35:14 -0500

    added links

--- scripts/main.js
+++ scripts/main.js
@@ -6,28 +6,6 @@ myButton.onclick = () =>{
 	setUserName();
 }
 
-function setUserName(){
-	const myName = prompt('Please enter your name.');
-	localStorage.setItem('name', myName);
-	
-	myHeading.textContent = `About Me, ${myName}`;
-	
-	 if (!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
-  }
-  
-	if(!localStorage.getItem('name')){
-		setUserName();
-	}
-	else{
-		const storedName = localStorage.getItem('name');
-		myHeading.textContent = `About Me, ${storedName}`;
-	}
-}
-
 //changes images on click
 myImage.onclick = ()=> {
 	const mySrc = myImage.getAttribute('src');
